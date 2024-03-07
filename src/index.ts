17833f6970654b7368b103b537819161ce17a93b
Alan <alan.rente@gmail.com>
2024-03-07 20:19:09 -0300

    iniciando com alguns exemplos

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { config } from "dotenv";
+config();
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+import { sequelize } from "./infra/database";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// (async () => {
+//   await alphavantageService({ symbol: "mxrf11" });
+//   process.exit(1);
+// })();
+
+try {
+  sequelize.authenticate().then(async () => {
+    // await sequelize.sync({ alter: true });
+
+    console.debug("Conectado ao banco");
+  });
+} catch (error) {
+  console.error("Não foi possível conectar ao banco", error);
+}
+
+app.get("/", (req, res) => {
+  res.json({ message: "rota / metodo get" });
+});
+
+app.use("/api", routes);
+
+const port = process.env.port;
+
+app.listen(port, () => {
+  console.debug(`http://localhost:${port}`);
+});
---
+++ src/models/CategoriaModel.ts
@@ -0,0 +1,20 @@
+// import { DataTypes } from "sequelize";
+// import { sequelize } from "../infra/database";
+
+// const CategoriaModel = sequelize.define(
+//   "CATEGORIA_TB",
+//   {
+//     categoriaId: {
+//       type: DataTypes.INTEGER,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     categoria: {
+//       type: DataTypes.STRING,
+//       unique: true,
+//     },
+//   },
+//   { freezeTableName: true }
+// );
+
+// export default CategoriaModel;
