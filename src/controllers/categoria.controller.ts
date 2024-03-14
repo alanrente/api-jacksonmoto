a5bd9cf7a3ba4aa163015cacb61e1ab9cf403bcc
Alan <alan.rente@gmail.com>
2024-03-13 23:40:50 -0300

    organizando

--- src/controllers/categoria.controller.ts
+++ src/controllers/categoria.controller.ts
@@ -1,32 +1,30 @@
 import { Request, Response } from "express";
-import Conexao from "../infra/database";
+import conexao from "../infra/database";
 import getCategoriaModel from "../models/CategoriaModel";
 
 export const CategoriaController = {
   async get(req: Request, res: Response) {
-    const conn = new Conexao();
     try {
-      const categoriaModel = await getCategoriaModel(conn.sequelize);
+      const conn = await conexao();
+      const categoriaModel = await getCategoriaModel(conn);
       const result = await categoriaModel.findAll();
+      conn.close();
       return res.send(result);
     } catch (error: any) {
+      console.log(error);
       return res.send({ message: error.message });
-    } finally {
-      try {
-        await conn.close();
-      } catch (err: any) {
-        console.log("Não há conexao para fechar");
-      }
     }
   },
   async post(req: Request, res: Response) {
-    const conn = new Conexao();
     try {
       const { categoria } = req.body;
-      const categoriaModel = await getCategoriaModel(conn.sequelize);
+      const conn = await conexao();
+      const categoriaModel = await getCategoriaModel(conn);
       const categoriaCreated = await categoriaModel.create({ categoria });
+      conn.close();
       return res.status(201).send(categoriaCreated);
     } catch (error: any) {
+      console.log(error);
       return res.status(500).send({ message: error.message });
     }
   },
--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,34 +1,24 @@
 import { Sequelize } from "sequelize";
 
-class Conexao {
-  sequelize: Sequelize;
-  static numero = 0;
-  constructor() {
-    Conexao.numero++;
-    const { database, username, password, host } = {
-      database: `${process.env.DB_DATABASE}`,
-      username: `${process.env.DB_USERNAME}`,
-      password: `${process.env.DB_PASSWORD}`,
-      host: `${process.env.DB_HOST}`,
-    };
-    this.sequelize = new Sequelize(database, username, password, {
+const { database, username, password, host } = {
+  database: `${process.env.DB_DATABASE}`,
+  username: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  host: `${process.env.DB_HOST}`,
+};
+
+export default async () => {
+  try {
+    const sequelize = new Sequelize(database, username, password, {
       dialect: "postgres",
       host: host,
       schema: process.env.DB_SCHEMA,
     });
-    console.log(`Conexão[${Conexao.numero}] aberta!`);
+    await sequelize.authenticate().then(() => {
+      console.log("banco conectado!");
+    });
+    return sequelize;
+  } catch (err: any) {
+    throw new Error(err.message);
   }
-
-  async close() {
-    await this.sequelize
-      .close()
-      .then(() => {
-        console.log(`Conexão[${Conexao.numero}] fechada!`);
-      })
-      .catch((err: any) => {
-        console.log("error: ", JSON.stringify(err));
-      });
-  }
-}
-
-export default Conexao;
+};
